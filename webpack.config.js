c1fbdfc578529e66b0e9ce2d7fbf1113cce8d6cd
Yushi <yushari@seznam.cz>
2026-02-28 12:52:09 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(process.cwd(), "docs"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource", // Handles weather icons automatically
+      },
+    ],
+  },
+};
