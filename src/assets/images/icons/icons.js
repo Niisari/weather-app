31216bd604f6d0ecd91b73a8844055a0dfd20d02
Yushi <yushari@seznam.cz>
2026-03-01 14:19:39 +0100

    Added missing icons

--- src/assets/images/icons/icons.js
+++ src/assets/images/icons/icons.js
@@ -2,9 +2,13 @@ import iconLoading from "./icon-loading.svg";
 import iconSearch from "./icon-search.svg";
 import iconUnits from "./icon-units.svg";
 import iconRetry from "./icon-retry.svg";
+import iconError from "./icon-error.svg";
+import iconCheckmark from "./icon-checkmark.svg";
+import iconDropdown from "./icon-dropdown.svg";
 
 import iconOvercast from "./icon-overcast.webp";
 import iconPartlyCloudy from "./icon-partly-cloudy.webp";
+import iconDrizzle from "./icon-drizzle.webp";
 import iconRain from "./icon-rain.webp";
 import iconStorm from "./icon-storm.webp";
 import iconSnow from "./icon-snow.webp";
@@ -21,4 +25,7 @@ export default {
   iconSnow,
   iconSunny,
   iconRetry,
+  iconError,
+  iconCheckmark,
+  iconDropdown,
 };
