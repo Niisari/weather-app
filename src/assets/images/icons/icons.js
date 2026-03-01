58256e050bb8d129d012ff028e8ec260e896365a
Yushi <yushari@seznam.cz>
2026-03-01 14:13:57 +0100

    Create data point for icons

---
+++ src/assets/images/icons/icons.js
@@ -0,0 +1,26 @@
+import iconLoading from "./icon-loading.svg";
+import iconRetry from "./icon-retry.svg";
+import iconSearch from "./icon-search.svg";
+import iconUnits from "./icon-units.svg";
+
+import iconOvercast from "./icon-overcast.webp";
+import iconPartlyCloudy from "./icon-partly-cloudy.webp";
+import iconRain from "./icon-rain.webp";
+import iconStorm from "./icon-storm.webp";
+import iconSnow from "./icon-snow.webp";
+import iconSunny from "./icon-sunny.webp";
+import iconRetry from "./icon-retry.webp";
+
+export default {
+  iconLoading,
+  iconRetry,
+  iconSearch,
+  iconUnits,
+  iconOvercast,
+  iconPartlyCloudy,
+  iconRain,
+  iconStorm,
+  iconSnow,
+  iconSunny,
+  iconRetry,
+};
