1b7837fa92e5d2c1c5278bae9104e12f05ab3780
Yushi <yushari@seznam.cz>
2026-03-01 14:28:58 +0100

    Added logo and units button to the header

--- src/pages/weather-app.js
+++ src/pages/weather-app.js
@@ -12,15 +12,22 @@ export class WeatherApp {
       <header class="header__wrapper">
         <div class="header__container">
           <div class="header__logo">
-            <img src="${logo}" alt="logo"/>
-            <p>Weather Now</p>
+            <img 
+            src="${logo}" 
+            alt="logo"/>
           </div>
 
           <div class="units__container">
             <button class="units__button">
               <img src="${icons.iconUnits}" alt="units icon"/>
               <p class="units__text">Units</p>
-              <img src="${icons.iconDropdown}" alt="dropdown icon"/>
+              <svg 
+              width="14" 
+              height="11" 
+              fill="none" 
+              viewBox="0 0 14 11">
+              <path fill="#fff" d="M11.895 1.047c.136-.137.355-.137.464 0l.793.766c.11.136.11.355 0 .464L4.95 10.48a.315.315 0 0 1-.465 0L.82 6.844c-.11-.137-.11-.356 0-.465l.793-.793c.11-.11.328-.11.465 0l2.625 2.652 7.192-7.191Z"/>
+              </svg>
             </button>
           </div>
         </div>
