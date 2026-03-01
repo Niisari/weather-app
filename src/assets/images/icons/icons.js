36385c4ef6fe514de7071eeb910e750b1095a1eb
Yushi <yushari@seznam.cz>
2026-03-01 14:16:57 +0100

    fixed duplicate

--- src/assets/images/icons/icons.js
+++ src/assets/images/icons/icons.js
@@ -1,7 +1,7 @@
 import iconLoading from "./icon-loading.svg";
-import iconRetry from "./icon-retry.svg";
 import iconSearch from "./icon-search.svg";
 import iconUnits from "./icon-units.svg";
+import iconRetry from "./icon-retry.svg";
 
 import iconOvercast from "./icon-overcast.webp";
 import iconPartlyCloudy from "./icon-partly-cloudy.webp";
@@ -9,11 +9,9 @@ import iconRain from "./icon-rain.webp";
 import iconStorm from "./icon-storm.webp";
 import iconSnow from "./icon-snow.webp";
 import iconSunny from "./icon-sunny.webp";
-import iconRetry from "./icon-retry.webp";
 
 export default {
   iconLoading,
-  iconRetry,
   iconSearch,
   iconUnits,
   iconOvercast,
