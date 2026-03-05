7e401d06dd85427020bb2d7e10b239c8f1bd6e12
Yushi <yushari@seznam.cz>
2026-03-05 14:24:47 +0100

    Created HourlyMenu Dropdown

---
+++ src/components/HourlyMenu.js
@@ -0,0 +1,15 @@
+export class HourlyMenu {
+  static render() {
+    return `
+    <div class="hourly__dropdown" id="hourly-dropdown">
+        <span>Monday</span>
+        <span>Tuesday</span>
+        <span>Wednesday</span>
+        <span>Thursday</span>
+        <span>Friday</span>
+        <span>Saturday</span>
+        <span>Sunday</span>
+    </div>
+    `;
+  }
+}
