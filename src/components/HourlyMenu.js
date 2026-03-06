e85c9fedd5b9c4e0b69511244fff5ebace65b489
Yushi <yushari@seznam.cz>
2026-03-06 18:36:36 +0100

    Removed static content for styling purposes with dynamic hourly dropdown connected to the api

--- src/components/HourlyMenu.js
+++ src/components/HourlyMenu.js
@@ -1,15 +1,25 @@
+// HourlyMenu.js
 export class HourlyMenu {
-  static render() {
+  static render(days = []) {
     return `
-    <div class="hourly__dropdown" id="hourly-dropdown">
-        <span class="hourly__day">Monday</span>
-        <span class="hourly__day">Tuesday</span>
-        <span class="hourly__day">Wednesday</span>
-        <span class="hourly__day">Thursday</span>
-        <span class="hourly__day">Friday</span>
-        <span class="hourly__day">Saturday</span>
-        <span class="hourly__day">Sunday</span>
-    </div>
+      <div class="hourly__dropdown" id="hourly-dropdown">
+        ${days
+          .slice(0, 7)
+          .map((day, index) => {
+            const dayName =
+              index === 0
+                ? "Today"
+                : new Date(day.datetime).toLocaleDateString("en-US", {
+                    weekday: "long",
+                  });
+            return `
+            <button class="hourly__option" type="button" data-index="${index}">
+              ${dayName}
+            </button>
+          `;
+          })
+          .join("")}
+      </div>
     `;
   }
 }
